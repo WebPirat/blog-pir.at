63cb2c750b19be4b0d1cd47dd8d6b71d0b1a4656
Dieter Koch <103990262+WebPirat@users.noreply.github.com>
2024-03-29 06:21:20 +0100

    updates

--- components/blog/writeBlogComment.vue
+++ components/blog/writeBlogComment.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import ValidMessage from "~/components/validate/validMessage.vue";
-import {validate} from "@babel/types";
 
 const supabase = useSupabaseClient();
 const name = ref('');
