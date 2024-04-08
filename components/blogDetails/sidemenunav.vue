d0ac73a3fe705309185989c9d3c4cbbd36f18db4
Dieter Koch <103990262+WebPirat@users.noreply.github.com>
2024-04-08 08:21:31 +0200

    updates braucht das land

--- components/blogDetails/sidemenunav.vue
+++ components/blogDetails/sidemenunav.vue
@@ -2,7 +2,7 @@
 import {defineComponent} from 'vue'
 
 export default defineComponent({
-  name: "sidemenu",
+  name: "sidemenunav",
   props:{
     blogid: {
       type: Number,
@@ -54,54 +54,67 @@ export default defineComponent({
 })
 </script>
 
+
 <template>
-  <div class="sticky-top fixed">
-    <div class="text-center space-x-2 text-2xl vertical-nav">
+  <div class="sticky-top fixed flex ">
+    <!-- Vertikale Links -->
+    <div class="vertical-links">
       <a v-if="showBarsLink" href="" @click.prevent="toggleBarsDiv"> <font-awesome-icon :icon="['fas', 'bars']" /> </a>
       <a v-if="showInfoLink" href="" @click.prevent="toggleInfoDiv"> <font-awesome-icon :icon="['fas', 'circle-info']" /> </a>
       <a href="#comments"> <font-awesome-icon :icon="['fas', 'comment']" /> </a>
       <a href="" @click.prevent="toggleShareDiv"> <font-awesome-icon :icon="['fas', 'share']" /></a>
     </div>
-    <div v-if="showBarsDiv" class="fade border border-lightgray rounded p-4 my-2">
-      <div class="text-center font-bold">Navigation</div>
-      <ol style="list-style: auto" class="mx-4">
-        <li v-for="nav in blognav" :key="nav.id">
-          <a :href="nav.link">{{ nav.title }}</a>
-        </li>
-      </ol>
-    </div>
-    <div v-if="showInfoDiv" class="fade border border-lightgray rounded p-4 my-2">
-      <div class="text-center font-bold mb-2">Infos</div>
-      <div v-for="info in bloginfo" :key="info.id" class="grid grid-cols-2">
-        <div>{{ info.title }}</div>
-        <div>{{ info.value }}</div>
+
+    <!-- Boxen rechts davon -->
+    <div class="flex-grow ">
+      <div v-if="showBarsDiv" class="fade border border-lightgray rounded p-4 my-2 bg-white">
+        <div class="text-center font-bold">Navigation</div>
+        <ol style="list-style: auto" class="mx-4">
+          <li v-for="nav in blognav" :key="nav.id">
+            <a :href="nav.link">{{ nav.title }}</a>
+          </li>
+        </ol>
       </div>
-    </div>
-    <div v-if="showShareDiv" class="fade text-white flex p-4">
-      <SocialShare
-          v-for="network in ['facebook', 'reddit', 'pinterest', 'twitter', 'linkedin', 'email']"
-          :key="network"
-          :network="network"
-          :styled="true"
-          :label="false"
-          class="p-4 rounded-none"
-      />
+      <div v-if="showInfoDiv" class="fade border border-lightgray rounded p-4 my-2 bg-white">
+        <div class="text-center font-bold mb-2">Infos</div>
+        <div v-for="info in bloginfo" :key="info.id" class="grid grid-cols-2">
+          <div>{{ info.title }}</div>
+          <div>{{ info.value }}</div>
+        </div>
+      </div>
+      <div v-if="showShareDiv" class="fade text-white flex p-4">
+        <SocialShare
+            v-for="network in ['facebook', 'reddit', 'pinterest', 'twitter', 'linkedin', 'email']"
+            :key="network"
+            :network="network"
+            :styled="true"
+            :label="false"
+            class="md:p-4 rounded"
+        />
+      </div>
     </div>
   </div>
 </template>
 
 <style scoped>
+  .vertical-links {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: flex-start;
+    padding-right: 10px; /* Abstand zwischen Links und Boxen */
+  }
 
-.sticky-top {
-  position: -webkit-sticky;
-  position: sticky;
-  top: 0;
-  z-index: 1000;
-}
+  .flex-grow {
+    flex-grow: 1;
+    padding-left: 10px; /* Abstand zwischen Links und Boxen */
+  }
+
 @media (max-width: 640px) {
   .sticky-top {
-    position:absolute;
-    top: 0;
+    position:fixed;
+    bottom: 70px;
+    left: 35px
   }
   .vertical-nav {
     display: flex;
